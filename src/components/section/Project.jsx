39421f51b6215ff16027d7bae8ca527741552ea1
Ngoc <lengockhanhdang@gmail.com>
2025-03-11 16:43:31 +0100

    change

--- src/components/section/Project.jsx
+++ src/components/section/Project.jsx
@@ -40,7 +40,7 @@ export const Project = () => {
             <p className="mb-4 text-gray-400">It features projects I have built using YouTube tutorials and my own creativity. I am passionate about coding, always eager to improve, and aspire to become a Full-Stack Developer.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {["61% CSS", "40% HTML"].map((tech,key) => (
+              {["94% JavaScript","3.2% CSS", "2.8% HTML"].map((tech,key) => (
                 <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[59,130,246,0.1] transition-all" key={key}>
                   {tech}
                 </span>
